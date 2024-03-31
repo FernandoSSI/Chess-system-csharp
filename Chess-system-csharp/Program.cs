2953c009a81e0b7d3c177ccabe992695a38ce097
FernandoSSI <fernandosi2005@gmail.com>
2024-03-31 10:52:10 -0300

    create Position class

--- Chess-system-csharp/Program.cs
+++ Chess-system-csharp/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Chess_system_csharp
+﻿using boardgame;
+
+namespace Chess_system_csharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
         }
     }
 }
---
+++ Chess-system-csharp/boardgame/Position.cs
@@ -0,0 +1,25 @@
+﻿namespace boardgame
+{
+    internal class Position
+    {
+
+        public int row {  get; set; }
+        public int col { get; set; }
+
+        public Position()
+        {
+            
+        }
+
+        public Position(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+        }
+
+        public override string ToString()
+        {
+            return row + ", " + col;
+        }
+    }
+}
