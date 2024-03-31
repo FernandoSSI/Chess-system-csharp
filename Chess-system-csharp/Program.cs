2e4bdc387c67d7f73735b69e0af7660e17c1fdec
FernandoSSI <fernandosi2005@gmail.com>
2024-03-31 10:37:09 -0300

    initial commit

---
+++ Chess-system-csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chess_system_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
